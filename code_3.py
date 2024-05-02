28543e9755391cf7e08b0d8906dc41b9e3d31f64
zhiavolnov <152493447+zhiavolnov@users.noreply.github.com>
2024-05-02 06:45:49 +0000

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,29 @@
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+        }
+
+        int[] data = new int[10];
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+
+    }
+        }
+public class RandomDataGenerator {
+}
+    public static void main(String[] args) {
+    const data = [];
+
+main();
+    return data;
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    for (let i = 0; i < 10; i++) {
+function main() {
+    });
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+
+    const data = generateRandomData();
+}
+function generateRandomData() {
